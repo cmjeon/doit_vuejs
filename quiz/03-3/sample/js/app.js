13f0b96b966caaa5281b3f86aaa1ca618aa2e0a2
cmjeon <cmjeon@lotte.net>
2020-04-01 11:04:33 +0900

    03-3과제수행

--- quiz/03-3/sample/js/app.js
+++ quiz/03-3/sample/js/app.js
@@ -1,6 +1,10 @@
 // 할일 #1
 // sibling-component 를 이름으로 갖는 새로운 컴포넌트를 아래에 등록해보세요.
 // options : template, props
+Vue.component('sibling-component', {
+  props: ['compdata'],
+  template: '<p>{{ compdata }}</p>'
+})
 
 Vue.component('child-component', {
   props: ['propsdata'],
@@ -10,10 +14,11 @@ Vue.component('child-component', {
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue! passed from Parent Component',
+    message: 'message! passed from Parent Component',
 
     // 할일 #2
     // data 속성을 1개 더 지정하고 (예: anotherMessage) 임의의 문자열을 값으로 대입해보세요.
     // 새로 지정한 data 속성은 위 sibling-component에 props로 전달합니다.
+    anoMsg: 'anoMsg! passed from Parent Component'
   }
 });
